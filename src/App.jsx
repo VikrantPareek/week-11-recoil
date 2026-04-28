a7e7770f5e62bc516b37c612ab34f4ff3a796bef
Vikrant Pareek <vikrantpareek2005@gmail.com>
2026-04-28 16:06:46 +0530

    Used recoil for state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,110 @@
-import { createContext, useContext, useState } from "react";
+import { useState } from "react";
+import { atom, RecoilRoot, useRecoilValue, useSetRecoilState } from "recoil";
 
-let CountContext = createContext();
+let counterAtom = atom({
+  key: "counter",
+  default: 0,
+});
 
 function App() {
-  let [count, setCount] = useState(0);
-
   return (
-    <CountContext.Provider value={{count, setCount}}>
+    <RecoilRoot>
       <Counter />
-    </CountContext.Provider>
+    </RecoilRoot>
   );
 }
 
-function Counter(){
-  return <div>
-    <Increase/>
-    <Decrease/>
-    <ShowCount/>
-  </div>
+function Counter() {
+  return (
+    <div>
+      <Increase />
+      <Decrease />
+      <ShowCount />
+    </div>
+  );
 }
 
-function Increase(){
-  let {count, setCount} = useContext(CountContext)
+function Increase() {
+  let setCount = useSetRecoilState(counterAtom);
 
-  function increase(){
-    setCount(count + 1)
+  function increase() {
+    setCount((c) => c + 1);
   }
 
-  return <button onClick={increase}>Increase Count</button>
+  return <button onClick={increase}>Increase Count</button>;
 }
 
-function Decrease(){
-  let {count, setCount} = useContext(CountContext)
+function Decrease() {
+  let setCount = useSetRecoilState(counterAtom);
 
-  function decrease(){
-    setCount(count - 1)
+  function decrease() {
+    setCount((c) => c - 1);
   }
 
-  return <button onClick={decrease}>Decrease Count</button>
+  return <button onClick={decrease}>Decrease Count</button>;
 }
 
-function ShowCount(){
-  let {count} = useContext(CountContext)
+function ShowCount() {
+  let count = useRecoilValue(counterAtom);
 
-  return <h1>{count}</h1>
+  return <h1>{count}</h1>;
 }
 
 export default App;
+
+
+
+
+
+
+
+
+// import { createContext, useContext, useState } from "react";
+
+// let CountContext = createContext();
+
+// function App() {
+//   let [count, setCount] = useState(0);
+
+//   return (
+//     <CountContext.Provider value={{count, setCount}}>
+//       <Counter />
+//     </CountContext.Provider>
+//   );
+// }
+
+// function Counter(){
+//   return <div>
+//     <Increase/>
+//     <Decrease/>
+//     <ShowCount/>
+//   </div>
+// }
+
+// function Increase(){
+//   let {count, setCount} = useContext(CountContext)
+
+//   function increase(){
+//     setCount(count + 1)
+//   }
+
+//   return <button onClick={increase}>Increase Count</button>
+// }
+
+// function Decrease(){
+//   let {count, setCount} = useContext(CountContext)
+
+//   function decrease(){
+//     setCount(count - 1)
+//   }
+
+//   return <button onClick={decrease}>Decrease Count</button>
+// }
+
+// function ShowCount(){
+//   let {count} = useContext(CountContext)
+
+//   return <h1>{count}</h1>
+// }
+
+// export default App;
