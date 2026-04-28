984590f49f08a7c3a2ca49da0b930c9d336e68e9
Vikrant Pareek <vikrantpareek2005@gmail.com>
2026-04-28 21:50:19 +0530

    Used selector in recoil

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,119 @@
-import { memo, useEffect, useState } from "react";
+import { atom, RecoilRoot, selector, useRecoilValue, useSetRecoilState } from "recoil";
+
+let counterAtom = atom({
+  key: "counter",
+  default: 0
+})
+
+let counterSelector = selector({
+  key: "selector",
+  get: ({get})=>{
+    let atom = get(counterAtom);
+    let isEven = (atom % 2 == 0);
+    return isEven;
+  }
+})
 
 function App(){
-  return <Counter/>
+  return <RecoilRoot>
+    <Counter/>
+  </RecoilRoot>
 }
 
 function Counter(){
-  let [count, setCount] = useState(0)
-
-  useEffect(()=>{
-    setInterval(()=>{
-      setCount(c => c + 1)
-    }, 3000)
-  }, [])
-
   return <div>
-    <h1>Count is {count}</h1>
-    <FirstChild/>
-    <SecondChild/>
-    <ThirdChild/>
+    <ShowCount/>
+    <Increase/>
+    <Decrease/>
+    <Even/>
   </div>
 }
 
-let FirstChild = memo(function (){
-  return <div>
-    This is the First Child Component
-  </div>
-})
+function ShowCount(){
+  let count = useRecoilValue(counterAtom);
 
-let SecondChild = memo(function (){
-  return <div>
-    This is the Second Child Component
-  </div>
-})
+  return <h1>{count}</h1>
+}
 
-let ThirdChild = memo(function (){
+function Increase(){
+  let setCount = useSetRecoilState(counterAtom);
+
+  function increase(){
+    setCount(c=>c+2)
+  }
+
+  return <button onClick={increase}>Increase</button>
+}
+
+function Decrease(){
+  let setCount = useSetRecoilState(counterAtom);
+
+  function decrease(){
+    setCount(c=>c-1)
+  }
+
+  return <button onClick={decrease}>Decrease</button>
+}
+
+function Even(){
+  let isEven = useRecoilValue(counterSelector);
+
   return <div>
-    This is the Third Child Component
+    {isEven ? "Even" : "Odd"}
   </div>
-})
+}
 
 export default App;
+
+
+
+
+
+
+
+
+// import { memo, useEffect, useState } from "react";
+
+// function App(){
+//   return <Counter/>
+// }
+
+// function Counter(){
+//   let [count, setCount] = useState(0)
+
+//   useEffect(()=>{
+//     setInterval(()=>{
+//       setCount(c => c + 1)
+//     }, 3000)
+//   }, [])
+
+//   return <div>
+//     <h1>Count is {count}</h1>
+//     <FirstChild/>
+//     <SecondChild/>
+//     <ThirdChild/>
+//   </div>
+// }
+
+// let FirstChild = memo(function (){
+//   return <div>
+//     This is the First Child Component
+//   </div>
+// })
+
+// let SecondChild = memo(function (){
+//   return <div>
+//     This is the Second Child Component
+//   </div>
+// })
+
+// let ThirdChild = memo(function (){
+//   return <div>
+//     This is the Third Child Component
+//   </div>
+// })
+
+// export default App;
 
 
 
