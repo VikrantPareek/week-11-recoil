16dfa46b65534a2872c4c6f62fd5ad83d6055ec1
Vikrant Pareek <vikrantpareek2005@gmail.com>
2026-04-28 14:52:44 +0530

    Revised the ContextAPI

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,49 @@
+import { createContext, useContext, useState } from "react";
+
+let CountContext = createContext();
+
 function App() {
-  return <div>Hello</div>
+  let [count, setCount] = useState(0);
+
+  return (
+    <CountContext.Provider value={{count, setCount}}>
+      <Counter />
+    </CountContext.Provider>
+  );
 }
 
-export default App
+function Counter(){
+  return <div>
+    <Increase/>
+    <Decrease/>
+    <ShowCount/>
+  </div>
+}
+
+function Increase(){
+  let {count, setCount} = useContext(CountContext)
+
+  function increase(){
+    setCount(count + 1)
+  }
+
+  return <button onClick={increase}>Increase Count</button>
+}
+
+function Decrease(){
+  let {count, setCount} = useContext(CountContext)
+
+  function decrease(){
+    setCount(count - 1)
+  }
+
+  return <button onClick={decrease}>Decrease Count</button>
+}
+
+function ShowCount(){
+  let {count} = useContext(CountContext)
+
+  return <h1>{count}</h1>
+}
+
+export default App;
